099922f97c3d8721b3826cc1129dc06709444e89
recih <recih@users.noreply.github.com>
2022-01-21 17:54:32 +0800

    initial commit

---
+++ Assets/BackgroundDownloadManager.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Unity.Networking;
+using UnityEngine;
+
+namespace BackgroundDownloadSample
+{
+    public class BackgroundDownloadManager : MonoBehaviour
+    {
+        public enum State
+        {
+            Idle,
+            Downloading,
+            Finished,
+        }
+        
+        private BackgroundDownload[] _downloads;
+        
+        public float updateInterval = 0.5f;
+        
+        public float TotalProgress { get; set; }
+
+        private State _state;
+        public State CurrentState
+        {
+            get => _state;
+            private set
+            {
+                if (_state != value)
+                {
+                    _state = value;
+                    OnStateChanged(_state);
+                    StateChanged?.Invoke(_state);
+                }
+            }
+        }
+
+        public event Action<State> StateChanged;
+
+        private void Start()
+        {
+            TotalProgress = 0;
+            _timer = 0;
+            _downloads = BackgroundDownload.backgroundDownloads;
+            CurrentState = _downloads.Length > 0 ? State.Downloading : State.Idle;
+        }
+
+        private void OnStateChanged(State state)
+        {
+            switch (state)
+            {
+                case State.Idle:
+                    TotalProgress = 0;
+                    _timer = 0;
+                    break;
+                case State.Downloading:
+                    Debug.Log($"Start downloading {_downloads.Length} files.");
+                    break;
+                case State.Finished:
+                    var successCount = 0;
+                    BackgroundDownload firstErrorDownload = null; 
+                    foreach (var download in _downloads)
+                    {
+                        if (download.status == BackgroundDownloadStatus.Done)
+                        {
+                            successCount++;
+                        }
+
+                        if (firstErrorDownload == null && download.status == BackgroundDownloadStatus.Failed)
+                        {
+                            firstErrorDownload = download;
+                        }
+                    }
+
+                    if (successCount < _downloads.Length)
+                    {
+                        Debug.LogError($"Finished downloading files, {successCount}/{_downloads.Length} succeeded.");
+                        if (firstErrorDownload != null)
+                        {
+                            Debug.LogError($"First error: {firstErrorDownload.config.url} {firstErrorDownload.error}");
+                        }
+                    }
+                    else
+                    {
+                        Debug.Log($"Finished downloading files, {successCount}/{_downloads.Length} succeeded.");
+                    }
+                    break;
+            }
+        }
+
+        private float _timer;
+
+        private void Update()
+        {
+            if (_timer > updateInterval)
+            {
+                _timer = 0;
+                UpdateDownload();
+            }
+            else
+            {
+                _timer += Time.deltaTime;
+            }
+        }
+
+        private void UpdateDownload()
+        {
+            if (_downloads == null || _downloads.Length <= 0) return;
+            
+            float totalProgress = 0;
+            bool finished = true;
+            foreach (BackgroundDownload download in _downloads)
+            {
+                totalProgress += download.progress;
+                if (download.status == BackgroundDownloadStatus.Downloading)
+                {
+                    finished = false;
+                }
+            }
+
+            TotalProgress = totalProgress / _downloads.Length;
+            CurrentState = finished ? State.Finished : State.Downloading;
+        }
+
+        private string ResolveFileNameFromUrl(Uri url)
+        {
+            return Path.GetFileName(url.AbsolutePath);
+        }
+
+        public void StartDownload(string destPath, List<string> urlList)
+        {
+            if (CurrentState != State.Idle)
+            {
+                Debug.LogError("Can't start download, current state is not idle.");
+                return;
+            }
+            
+            if (urlList.Count <= 0) return;
+
+            var configs = new List<BackgroundDownloadConfig>();
+            foreach (var url in urlList)
+            {
+                var uri = new Uri(url);
+                var fileName = ResolveFileNameFromUrl(uri);
+                var destFilePath = Path.Combine(destPath, fileName);
+                configs.Add(new BackgroundDownloadConfig{url = uri, filePath = destFilePath});
+            }
+
+            _downloads = BackgroundDownload.Start(configs.ToArray());
+            CurrentState = _downloads.Length > 0 ? State.Downloading : State.Idle;
+            TotalProgress = 0;
+            _timer = 0;
+        }
+
+        public void ClearAll()
+        {
+            if (_downloads == null) return;
+            
+            foreach (var download in _downloads)
+            {
+                download.Dispose();
+            }
+
+            _downloads = null;
+            CurrentState = State.Idle;
+        }
+    }
+}
---
+++ Assets/DownloadSample.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Unity.Networking;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace BackgroundDownloadSample
+{
+    public class DownloadSample : MonoBehaviour
+    {
+        private static string[] DownloadUrls = new[]
+        {
+            "https://dldir1.qq.com/weixin/Windows/WeChatSetup.exe",
+            "https://dldir1.qq.com/weixin/android/weixin8018android2060_arm64.apk",
+            "https://dldir1.qq.com/weixin/mac/WeChatMac.dmg"
+        };
+        
+        public string DestPath;
+        public BackgroundDownloadManager manager;
+        public Slider slider;
+        public Button startButton;
+        public Button clearButton;
+
+        private void Start()
+        {
+            if (manager != null)
+            {
+                manager.StateChanged += OnStateChanged;
+                UpdateButtonState(manager.CurrentState);
+            }
+            if (startButton != null)
+            {
+                startButton.onClick.AddListener(StartDownload);
+            }
+            if (clearButton != null)
+            {
+                clearButton.onClick.AddListener(ClearDownload);
+            }
+        }
+        
+        private string AbsoluteDestPath => Path.Combine(Application.persistentDataPath, DestPath);
+
+        private void StartDownload()
+        {
+            if (manager == null) return;
+            
+            Directory.CreateDirectory(AbsoluteDestPath);
+            manager.StartDownload(DestPath, new List<string>(DownloadUrls));
+        }
+        
+        private void ClearDownload()
+        {
+            if (manager == null) return;
+            
+            manager.ClearAll();
+        }
+
+        private void OnStateChanged(BackgroundDownloadManager.State state)
+        {
+            UpdateButtonState(state);
+
+            switch (state)
+            {
+                case BackgroundDownloadManager.State.Finished:
+                    Debug.Log($"Finished download, dest path: {AbsoluteDestPath}");
+                    break;
+            }
+        }
+
+        private void UpdateButtonState(BackgroundDownloadManager.State state)
+        {
+            if (startButton != null)
+            {
+                startButton.interactable = state == BackgroundDownloadManager.State.Idle;
+            }
+
+            if (clearButton != null)
+            {
+                clearButton.interactable = state == BackgroundDownloadManager.State.Finished;
+            }
+        }
+
+        private void Update()
+        {
+            if (manager == null || slider == null) return;
+
+            slider.value = manager.TotalProgress;
+        }
+    }
+}
